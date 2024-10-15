c292207196e6125ab38495348ce8fc313076560d
Sevda Molani <37120666+SevdaMolani@users.noreply.github.com>
2024-10-15 14:12:15 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,24 +6,17 @@ Hospital Readmission Package
 This package contains tools for analyzing hospital readmission data, 
 including data preprocessing, feature engineering, and model training.
 
-Modules:
-- data_preprocessing: Functions for cleaning and preparing data.
-- feature_engineering: Tools for creating features from raw data.
-- model_training: Functions for training and evaluating machine learning models.
 """
 
 # Importing relevant functions or classes for easier access
-from .EDA import (
-    perform_eda
-)
+from .eda import perform_eda
+from .modeling_preprocessing import modeling_pipeline
 
 
-from .model_training import (
-    modeling_pipeline
-)
+perform_eda('/content/hospital_readmissions.csv')
+performance_table, classification_report_table = modeling_pipeline()
+print("Performance Table:")
+print(performance_table)
+print("\nClassification Report Table:")
+print(classification_report_table)
 
-# Define __all__ for cleaner imports
-__all__ = [
-    "perform_eda",
-    "modeling_pipeline"
-]
